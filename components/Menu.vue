ceebdd98c85ef30b1dd9e398eb66aed26b2ca00c
gebarb <ghebarb@gmail.com>
2024-10-23 18:20:33 -0500

    get menu working

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,14 +1,36 @@
+<script setup lang="ts">
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faBars);
+</script>
+
 <template>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <NuxtLink to="/">Home</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/faq">FAQ</NuxtLink>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    <nav class="navbar">
+        <button @click="toggleMenu" class="navbar-toggler">
+            <FontAwesomeIcon icon="fa-bars" class="navbar-toggle-icon"></FontAwesomeIcon>
+        </button>
+        <div class="navbar-menu collapse" :class="{ 'show': isMobileMenuOpen }">
+            <div class="navbar-nav">
+                <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+                <NuxtLink class="nav-link" to="/faq">FAQ</NuxtLink>
+            </div>
+        </div>
+    </nav>
 </template>
+
+<script lang="ts">
+export default {
+    data() {
+        return {
+            isMobileMenuOpen: false,
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.isMobileMenuOpen = !this.isMobileMenuOpen;
+        },
+    },
+};
+</script>
