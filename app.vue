dbf3dd39c9e472a9ca8f01cfd4042696e623129a
gebarb <ghebarb@gmail.com>
2024-10-26 15:13:44 -0500

    font styling

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Meow+Script&family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap");
+</style>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
@@ -6,5 +10,5 @@
       <Menu></Menu>
       <NuxtPage />
     </NuxtLayout>
-</div>
+  </div>
 </template>
