6b90d93523a53991c596ed9765ff0605922156f6
gebarb <ghebarb@gmail.com>
2024-10-28 21:12:28 -0500

    my first loop

---
+++ components/FaqItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>{{ faqItem.question }}</h3>
+    <p>{{ faqItem.answer }}</p>
+    <br>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+    faqItem: {
+        type: Object,
+        required: true,
+    },
+});
+
+const { faqItem } = props;
+
+</script>
--- pages/faq.vue
+++ pages/faq.vue
@@ -3,23 +3,26 @@
     <div class="content">
         <h1>Frequently Asked Questions</h1>
         <hr class="rounded">
-        <br>
-        <h3>Question 1</h3>
-        <p>Here is an answer to Question 1</p>
-        <br>
-        <h3>Question 2</h3>
-        <p>Here is an answer to Question 2</p>
-        <br>
-        <h3>Question 3</h3>
-        <p>Here is an answer to Question 3</p>
-        <br>    
-        <h3>Question 4</h3>
-        <p>Here is an answer to Question 4</p>
-</div>
+        <FaqItem v-for="faqItem in faq" :faqItem="faqItem" :key="faqItem.id">
+        </FaqItem>
+    </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
     layout: "faq"
 });
+
+const faq = [
+    { id: 1, question: "What Time Should I Arrive?", answer: "ANSWER" },
+    { id: 2, question: "Where Can I Stay?", answer: "ANSWER" },
+    { id: 3, question: "Are Kids Allowed?", answer: "ANSWER" },
+    { id: 4, question: "How Should I RSVP?", answer: "ANSWER" },
+    { id: 5, question: "Where Is The Registry?", answer: "ANSWER" },
+    { id: 6, question: "Will The Wedding Be Indoors?", answer: "ANSWER" },
+    { id: 7, question: "Will There Be Parking At The Venue?", answer: "ANSWER" },
+    { id: 8, question: "What Is The Dress Code?", answer: "ANSWER" },
+    { id: 9, question: "QUESTION", answer: "ANSWER" },
+    { id: 10, question: "QUESTION", answer: "ANSWER" },
+];
 </script>
