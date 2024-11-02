2deca498b2198700c6d881c3f71cbe0195a3a950
gebarb <ghebarb@gmail.com>
2024-11-01 20:24:59 -0500

    try embed map

--- components/Menu.vue
+++ components/Menu.vue
@@ -7,7 +7,8 @@
             <div class="navbar-nav">
                 <NuxtLink class="nav-link" to="/">Home</NuxtLink>
                 <NuxtLink class="nav-link" to="/faq">FAQ</NuxtLink>
-                <NuxtLink class="nav-link" to="https://www.amazon.com" target="_blank">Registry</NuxtLink>
+                <!-- <NuxtLink class="nav-link" to="https://www.amazon.com" target="_blank">Registry</NuxtLink> -->
+                <!-- <NuxtLink class="nav-link" to="https://www.google.com" target="_blank">RSVP</NuxtLink> -->
             </div>
         </div>
     </nav>
--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,9 @@
             <h3>
                 1467 FM 1200, Gainesville, TX 76240
             </h3>
+            <br>
+            <iframe class="google-map" allow="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3320.328371752484!2d-97.2043838410631!3d33.67456053487122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864d0742398581d1%3A0x98962f6eb613b9dd!2sThe%20Barn%20at%20Wolf%20Ridge%20Farm!5e0!3m2!1sen!2sus!4v1730509976598!5m2!1sen!2sus"></iframe>
         </div>
     </div>
 </template>
