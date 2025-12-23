efee5fc5d54268e0ddbeb185c910907e9ab44850
gebarb <ghebarb@gmail.com>
2025-12-23 10:18:07 -0600

    fix registry modal

--- components/RegistryModal.vue
+++ components/RegistryModal.vue
@@ -1,14 +1,14 @@
 <template>
     <div v-if="isOpen" class="modal-overlay" @click.self="$emit('close-modal')">
         <div class="modal" @click.stop>
-            <div>
-                <NuxtLink class="nav-link bold" :to="AMAZON_URL" target="_blank">Amazon</NuxtLink>
-                <br>
-                <NuxtLink class="nav-link bold" :to="CASHAPP_URL" target="_blank">CashApp</NuxtLink>
-                <br>
-                <NuxtLink class="nav-link bold" :to="VENMO_URL" target="_blank">Venmo</NuxtLink>
-                <div class="close" @click="$emit('close-modal')">
+            <div class="modal-content">
+                <div class="close" @click="$emit('close-modal')" aria-label="Close">
                     <font-awesome-icon icon="fa-solid fa-x" />
+                </div>
+                <div class="registry-links">
+                    <NuxtLink class="nav-link bold" :to="AMAZON_URL" target="_blank">Amazon</NuxtLink>
+                    <NuxtLink class="nav-link bold" :to="CASHAPP_URL" target="_blank">CashApp</NuxtLink>
+                    <NuxtLink class="nav-link bold" :to="VENMO_URL" target="_blank">Venmo</NuxtLink>
                 </div>
             </div>
         </div>
